323666fa2e912af47c3259684b49b544870ffb72
li-kang <likang2008@gmail.com>
2019-07-25 19:59:34 +0800

    完成微信登录

---
+++ src/api/api.js
@@ -0,0 +1,69 @@
+
+import mpx from '@mpxjs/core'
+
+class API {
+  constructor () {
+    ['delete', 'get', 'head', 'options', 'trace', 'post', 'put', 'patch'].forEach((method) => {
+      API.prototype[method] = function (url, data, config) {
+        return this.request({
+          ...config,
+          method,
+          url,
+          data
+        })
+      }
+    })
+  }
+
+  // 获取配置
+  config () {
+    return API._config.default
+  }
+
+  // 网络请求
+  request (options) {
+    wx.showNavigationBarLoading()
+
+    //  console.log('request', options)
+    return mpx.request({
+      ...options,
+      url: `${options.baseURL || this.config().baseURL}${options.url}`,
+      header: {
+        ...(this.config().headers)
+      }
+    }).then((res) => {
+      wx.hideNavigationBarLoading()
+      switch (res.statusCode) {
+        case 200:
+          return res.data
+        default:
+          console.warn(API.statusCode[res.statusCode])
+          throw res
+      }
+    })
+  }
+}
+
+// 默认请求配置
+API._config = {
+  default: {
+    baseURL: 'http://127.0.0.1:8000/api/v1/',
+    headers: {
+      'Authorization': ''
+      // 'Accept': 'application/json, text/plain, */*',
+      // 'Content-Type': 'application/x-www-form-urlencoded'
+      // 'Content-type': 'application/json',
+    }
+  }
+}
+
+API.statusCode = {
+  200: '正常请求',
+  401: '未登陆',
+  403: '未授权接口',
+  500: '服务器出错',
+  502: '服务器出错',
+  503: '哦～服务器宕机了'
+}
+
+export default API
